c58e6546bd1a3a9ba7c44abe9e4870a5d93ca858
Sandro Alvarez and Stephen Burden <sandro@>
2016-08-15 11:23:24 -0700

    add more js to sort items into a new sorted, capitalized list

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,19 @@ $(function() {
     event.preventDefault();
   });
 
+  $("form").submit(function(event) {
+    $(".row").slideUp();
+    groceries.sort();
+    var groceryList = groceries.map(function(item) {
+      return item.toUpperCase();
+    });
+    groceryList.forEach(function(grocery) {
+      $("#newList").append("<li>" + grocery + "</li>");
+    })
+    event.preventDefault();
+  });
+
+
 
 //  $("form").submit(function(event) {
 
